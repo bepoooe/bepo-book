3dc9bb82ab1a4ffede1f0f8b16c62c3ab2e4b422
Adrish Basak <adrishbasak003@gmail.com>
2024-09-22 18:54:26 +0530

    change 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/bepo-book/', // Add this line
+  base: '/<bepo-book>/', // Add this line
   plugins: [react()],
 });
